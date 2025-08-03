37703ae623f944f112fb3f95202907f4ddd87ace
tmdragon2 <133560385+tmdragon2@users.noreply.github.com>
2025-08-03 06:00:21 -0400

    Imported a buncha assets and finished main menu

--- gtmk-jam2025/Interactables/Objects/key.gd
+++ gtmk-jam2025/Interactables/Objects/key.gd
@@ -1,7 +1,11 @@
 extends Node2D
 var keytaken = false 
 var doorcheck = false 
-var checkwalls = false
+var checkwalls 
+@onready var raycast1 = $RayCast2D
+@onready var raycast2 = $RayCast2D2
+@onready var raycast3 = $RayCast2D3
+@onready var raycast4 = $RayCast2D4
 signal dooropened
 
 
@@ -15,8 +19,12 @@ func _on_player_detection_body_entered(body: Node2D) -> void:
 		if keytaken == false:
 			body.keytaken = true
 			SignalBus.open_door.emit()
-
-func _on_collide_detection_body_entered(body: Node2D) -> void:
-	checkwalls = true
-	if checkwalls == true:
-		print("i spawned in a wall")
+func walldetection():
+	if raycast1.is_colliding():
+		move_toward(1,1,1)
+	if raycast2.is_colliding():
+		move_toward(1,1,1)
+	if raycast3.is_colliding():
+		move_toward(1,1,1)
+	if raycast4.is_colliding():
+		move_toward(1,1,1)
