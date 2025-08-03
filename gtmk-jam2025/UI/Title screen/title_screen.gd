d1f61db4177d0ab1eb391288bb9958f3d3c3b4f4
tmdragon2 <133560385+tmdragon2@users.noreply.github.com>
2025-08-02 20:12:53 -0400

    fixed key

--- gtmk-jam2025/UI/Title screen/title_screen.gd
+++ gtmk-jam2025/UI/Title screen/title_screen.gd
@@ -5,7 +5,7 @@ extends Control
 func _ready() -> void:
 	options.visible = false
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://world nodes/level area.tscn")
+	get_tree().change_scene_to_file("res://world nodes/main_level.tscn")
 
 
 func _on_quit_pressed() -> void:
